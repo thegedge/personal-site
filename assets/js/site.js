3f86a8c24fcf4fe83d04f64014edafb507d331db
Jason Gedge <inferno1386@gmail.com>
2013-01-04 17:16:11 -0330

    Add Manatee and Jype projects to portfolio

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	$('body.portfolio #content > ul > li[id]').each(function() {
-		console.log(this);
 		$('a[rel*="lightbox"]', this).colorbox({
 			      rel: $(this).attr('id'),
 			 maxWidth: '95%',
