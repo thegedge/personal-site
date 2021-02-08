f452107b7ea6befb5eac7d6e90f56203740bbdbe
Jason Gedge <jason@gedge.ca>
2021-02-08 16:23:14 -0500

    Style some external links with appropriate icon

--- lib/components/Link.tsx
+++ lib/components/Link.tsx
@@ -1,3 +1,10 @@
+import {
+  faBitbucket,
+  faGithub,
+  faTwitter,
+  faWikipediaW,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { isExternalUrl } from "../utils";
@@ -6,11 +13,28 @@ export const Link = (props: { href: string; className?: string; children: React.
   const className =
     "text-primary-700 border-primary-300 border-b-2 hover:text-primary-400 transition-colors duration-200 ease-in-out";
 
-  if (isExternalUrl(props.href)) {
+  let icon;
+  if (props.href.includes("github.com")) {
+    icon = <FontAwesomeIcon style={{ marginBottom: "0.15em" }} icon={faGithub} size="xs" />;
+  } else if (props.href.includes("wikipedia.org")) {
+    icon = <FontAwesomeIcon style={{ marginBottom: "0.15em" }} icon={faWikipediaW} size="xs" />;
+  } else if (props.href.includes("youtube.com")) {
+    icon = <FontAwesomeIcon style={{ marginBottom: "0.15em" }} icon={faYoutube} size="sm" />;
+  } else if (props.href.includes("twitter.com")) {
+    icon = <FontAwesomeIcon style={{ marginBottom: "0.15em" }} icon={faTwitter} size="xs" />;
+  } else if (props.href.includes("bitbucket.org")) {
+    icon = <FontAwesomeIcon style={{ marginBottom: "0.15em" }} icon={faBitbucket} size="xs" />;
+  } else if (isExternalUrl(props.href)) {
+    icon = (
+      <FontAwesomeIcon style={{ marginBottom: "0.15em" }} icon={faExternalLinkAlt} size="xs" />
+    );
+  }
+
+  if (icon) {
     return (
       <a href={props.href} className={`${className} ${props.className}`}>
-        {props.children}&nbsp;
-        <FontAwesomeIcon style={{ marginBottom: "0.15em" }} icon={faExternalLinkAlt} size="xs" />
+        {props.children}
+        <span className="whitespace-nowrap">&nbsp;{icon}</span>
       </a>
     );
   } else {
