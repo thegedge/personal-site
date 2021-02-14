bea2e65f7bc2e3eb604ec07791fd50c6bb00434e
Jason Gedge <jason@gedge.ca>
2021-02-14 10:37:29 -0500

    Add script to parse markdown

---
+++ bin/parse_md.ts
@@ -0,0 +1,27 @@
+import frontmatter from "@github-docs/frontmatter";
+import fs from "fs";
+import { mapValues } from "lodash";
+import remarkDeflist from "remark-deflist";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import remarkParse from "remark-parse";
+import unified from "unified";
+
+function dropPositions(node: any) {
+  return mapValues(node, (value, key) => {
+    switch (key) {
+      case "position":
+        return undefined;
+      case "children":
+        return value.map(dropPositions);
+      default:
+        return value;
+    }
+  });
+}
+
+const contents = fs.readFileSync("/dev/stdin");
+const markdown = frontmatter(contents.toString()).content;
+const processor = unified().use(remarkParse).use(remarkGfm).use(remarkMath).use(remarkDeflist);
+const tree = processor.runSync(processor.parse(markdown));
+console.log(JSON.stringify(dropPositions(tree), null, 2));
